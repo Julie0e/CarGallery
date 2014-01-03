598af29307fa8853d8b8847a0003124d0d064db1
Julie0e <julie_91@hanmail.net>
2014-01-03 13:58:27 +0900

    14.1.2 carGallery
    첫실습과제 제출하

--- CarGallery/ViewController.m
+++ CarGallery/ViewController.m
@@ -9,21 +9,58 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    //NSArray *_image;
+    NSInteger _int;
+}
+@property (weak, nonatomic) IBOutlet UIImageView *imgView;
 
 @end
 
 @implementation ViewController
+- (IBAction)prev:(id)sender {
+    if (_int<=1) {
+        NSString *carName = [NSString stringWithFormat:@"car"];
+        NSString *imagePath = [[NSBundle mainBundle] pathForResource:carName ofType:@"png"];
+        UIImage *currentImage = [UIImage imageWithContentsOfFile:imagePath];
+        self.imgView.image = currentImage;
+    }
+    else
+        _int--;
+         [self carImagePath];
+}
+- (IBAction)next:(id)sender {
+    
+    if (_int >= 3) {
+        NSString *carName = [NSString stringWithFormat:@"car3"];
+        NSString *imagePath = [[NSBundle mainBundle] pathForResource:carName ofType:@"png"];
+        UIImage *currentImage = [UIImage imageWithContentsOfFile:imagePath];
+        self.imgView.image = currentImage;
+    }
+    else
+        _int++;
+        [self carImagePath];
+}
 
+-(void)carImagePath{
+    NSString *carName = [NSString stringWithFormat:@"car%d",(int)_int];
+    NSString *imagePath = [[NSBundle mainBundle] pathForResource:carName ofType:@"png"];
+    UIImage *currentImage = [UIImage imageWithContentsOfFile:imagePath];
+    self.imgView.image = currentImage;
+   
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    _int = 1;
+    [self carImagePath];
+	
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
 }
 
 @end
